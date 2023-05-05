cbe43f7efc32852514b42c8e6b07c56febfa555f
rangeran0304 <81308074+rangeran0304@users.noreply.github.com>
2023-05-05 00:02:43 -0400

    Update settings.py

--- twitter/settings.py
+++ twitter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '63u@v9)#e3#kmtkek_+4^%en^l16alocvz4pb9_m6jjdbi#1_v'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
